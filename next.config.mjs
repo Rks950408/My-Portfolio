e3ab646a8b925ef15c3aa183cb6d7ddc6c3c5a4a
rks950408 <rks950408@gmail.com>
2025-04-17 12:58:25 +0530

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
+// next.config.mjs
+export default {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-}
-
-export default nextConfig
+  basePath: '/My-Portfolio',
+  trailingSlash: true,
+};
